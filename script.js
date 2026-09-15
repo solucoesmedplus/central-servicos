f4fa77525a2530045cad6875c2c427aea0edf52f
solucoesmedplus <solucoesmedplus@gmail.com>
2026-09-15 16:45:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -126,3 +126,14 @@
     });
   });
 })();
+
+//PRELOADER
+// Remove o Preloader após o carregamento completo da página
+window.addEventListener('load', () => {
+    const preloader = document.getElementById('preloader');
+
+    // Pequeno delay para garantir que a renderização foi concluída
+    setTimeout(() => {
+        preloader.classList.add('loaded');
+    }, 1000);
+});
